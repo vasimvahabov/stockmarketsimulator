15bb70dbffa0b2904569284d0490a6cdfa0f6b51
Vasim <vahabov.vasim@gmail.com>
2026-03-31 00:18:57 +0200

    fix(client): add support to follow redirects

--- src/main/java/com/vasimvahabov/stockmarketsimulator/config/RestClientConfig.java
+++ src/main/java/com/vasimvahabov/stockmarketsimulator/config/RestClientConfig.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.ApiVersionInserter;
 import org.springframework.web.client.RestClient;
+import java.net.http.HttpClient;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,7 +21,14 @@ public class RestClientConfig {
 
     @Bean
     RestClient finnhubClient() {
+        HttpClient httpClient = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .build();
+
+        JdkClientHttpRequestFactory requestFactory = new JdkClientHttpRequestFactory(httpClient);
+
         return RestClient.builder()
+                .requestFactory(requestFactory)
                 .baseUrl(finnhubConfig.getBaseUrl())
                 .apiVersionInserter(ApiVersionInserter.usePathSegment(1))
                 .defaultApiVersion(finnhubConfig.getApiVersion())
