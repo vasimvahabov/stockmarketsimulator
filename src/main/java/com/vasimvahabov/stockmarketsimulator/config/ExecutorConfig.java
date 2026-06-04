f40b51bd38bb9ab7afad7b58cc48938acff93e26
Vasim <vahabov.vasim@gmail.com>
2026-06-04 22:26:01 +0200

    feat(executor): enable to configure stock executor threadPoolSize externally

--- src/main/java/com/vasimvahabov/stockmarketsimulator/config/ExecutorConfig.java
+++ src/main/java/com/vasimvahabov/stockmarketsimulator/config/ExecutorConfig.java
@@ -1,18 +1,29 @@
 package com.vasimvahabov.stockmarketsimulator.config;
 
-import java.util.concurrent.*;
-
 import com.vasimvahabov.stockmarketsimulator.constant.ExecutorThread;
+import com.vasimvahabov.stockmarketsimulator.scheduler.properties.StockSynchronizerProps;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.*;
+
+@Slf4j
 @Configuration
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ExecutorConfig {
 
-    @Bean(name = "stockExecutor", destroyMethod = "close")
-    public ScheduledExecutorService stockExecutor() {
+    StockSynchronizerProps stockSyncProps;
+
+    @Bean(name = "stockScheduledExecutor", destroyMethod = "close")
+    public ScheduledExecutorService stockScheduledExecutor() {
         return new ScheduledThreadPoolExecutor(
-                1, r -> new Thread(r, ExecutorThread.STOCK_SYNC.getThread())
+                stockSyncProps.getThreadPoolSize(),
+                r -> new Thread(r, ExecutorThread.STOCK_SYNC.getThread())
         );
     }
 
