1cb024262787f3e9a0f4651a059f0283cf0278ab
Vasim <vahabov.vasim@gmail.com>
2026-06-07 14:35:03 +0200

    chore(gradle): clean up gradle files

--- src/main/java/com/vasimvahabov/stockmarketsimulator/repository/CandleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vasimvahabov.stockmarketsimulator.repository;
-
-import com.vasimvahabov.stockmarketsimulator.entity.Candle;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CandleRepository extends JpaRepository<Candle, Long> {
-}
