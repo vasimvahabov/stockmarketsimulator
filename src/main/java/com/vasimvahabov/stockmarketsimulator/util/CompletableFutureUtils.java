659667c84ba3d37e185e525e56b08da00b7ade96
Vasim <vahabov.vasim@gmail.com>
2026-07-11 16:47:00 +0200

    feat(utility): add CompletableFutureUtils.logCompletion() that handles completion in whenComplete()

--- src/main/java/com/vasimvahabov/stockmarketsimulator/util/CompletableFutureUtils.java
+++ src/main/java/com/vasimvahabov/stockmarketsimulator/util/CompletableFutureUtils.java
@@ -26,4 +26,16 @@ public final class CompletableFutureUtils {
         };
     }
 
+    public static <T> BiConsumer<T, Throwable> logCompletion(
+            Logger log, String successMessage, String failureMessage
+    ) {
+        return (_, throwable) -> {
+            if (throwable != null) {
+                log.error("{}: {}", failureMessage, throwable.getMessage(), throwable);
+                return;
+            }
+            log.debug("{}", successMessage);
+        };
+    }
+
 }
