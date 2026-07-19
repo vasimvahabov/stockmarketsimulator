bc7e0c56e09fe53c584d2871d7c3a7eb44a64d8e
Vasim <vahabov.vasim@gmail.com>
2026-07-19 16:50:39 +0200

    refactor(service): adjust QuoteService.createQuotes to process list of ConsumerRecord

--- src/main/java/com/vasimvahabov/stockmarketsimulator/service/QuoteService.java
+++ src/main/java/com/vasimvahabov/stockmarketsimulator/service/QuoteService.java
@@ -4,6 +4,7 @@ import com.vasimvahabov.stockmarketsimulator.dto.response.QuoteWSResponse;
 import com.vasimvahabov.stockmarketsimulator.entity.Quote;
 import com.vasimvahabov.stockmarketsimulator.entity.Stock;
 import jakarta.annotation.Nonnull;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.time.Instant;
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.Map;
 
 public interface QuoteService {
 
-    void createQuotes(@Nonnull List<QuoteWSResponse> wsResponses, @Nonnull Map<String, Stock> stocksMap);
+    void createQuotes(@Nonnull List<ConsumerRecord<String, QuoteWSResponse>> records);
 
     Map<Stock, List<Quote>> findQuotesGroupedByStockSince(Instant timestampMs);
 
--- src/main/java/com/vasimvahabov/stockmarketsimulator/service/impl/QuoteServiceImpl.java
+++ src/main/java/com/vasimvahabov/stockmarketsimulator/service/impl/QuoteServiceImpl.java
@@ -11,6 +11,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 
@@ -28,32 +29,30 @@ public class QuoteServiceImpl implements QuoteService {
 
     QuoteMapper quoteMapper;
 
-    public void createQuotes(@NonNull List<QuoteWSResponse> wsResponses, @NonNull Map<String, Stock> stocksMap) {
-        try {
-            List<Quote> quotesToCreate = wsResponses
-                    .stream()
-                    .flatMap(wsResponse -> Optional.ofNullable(wsResponse.data())
-                            .orElseGet(Collections::emptyList).stream())
-                    .map(data -> quoteMapper.wsResponseToEntity(data, stocksMap))
-                    .toList();
+    StockService stockService;
 
-            if (quotesToCreate.isEmpty()) {
-                log.warn("No quotes to create from {} responses", wsResponses.size());
-                return;
-            }
-            log.info("Creating quotes: {} quotes", quotesToCreate.size());
-            List<Quote> savedQuotes = quoteRepository.saveAll(quotesToCreate);
-            log.info("Successfully created {} quotes", savedQuotes.size());
+    @Override
+    public void createQuotes(@NonNull List<ConsumerRecord<String, QuoteWSResponse>> records) {
+        try {
+            Map<String, Stock> stockMap = stockService.findStocks();
+            List<Quote> quotes = records.stream()
+                    .filter(record -> record.value().data()!=null)
+                    .flatMap(record -> record.value().data().stream())
+                    .map(data -> quoteMapper.wsResponseToEntity(data, stockMap))
+                    .toList();
+            log.info("Creating {} quotes", quotes.size());
+            quoteRepository.saveAll(quotes);
+            log.info("Successfully created {} quotes", quotes.size());
         } catch (Exception exception) {
-            log.error("Failed to save quotes to database: {}", exception.getMessage(), exception);
+            log.error("Failed to create quotes: {}", exception.getMessage(), exception);
+            throw exception;
         }
     }
-
 
     @Override
     public Map<Stock, List<Quote>> findQuotesGroupedByStockSince(Instant timestampMs) {
         log.info("Retrieving quotes since timestamp: {}", timestampMs);
-        Map<Stock, List<Quote>> quotes = quoteRepository
+        Map<Stock, List<Quote>> quoteMap = quoteRepository
                 .findAllByTimestampMsGreaterThanEqual(timestampMs)
                 .stream()
                 .collect(Collectors.collectingAndThen(
@@ -62,7 +61,7 @@ public class QuoteServiceImpl implements QuoteService {
                 ));
 
         log.info("Retrieved quotes successfully since timestamp: {}", timestampMs);
-        return quotes;
+        return quoteMap;
     }
 
 }
