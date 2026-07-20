5394e3ea504ec27a4ef623a9ba1ecb7835fc5c15
Vasim <vahabov.vasim@gmail.com>
2026-07-20 17:35:30 +0200

    feat(service): add saveQuotePublishCheckpoint to persist latest Finnhub WebSocket-to-Kafka quote publish checkpoints

---
+++ src/main/java/com/vasimvahabov/stockmarketsimulator/service/QuotePublishCheckpointService.java
@@ -0,0 +1,9 @@
+package com.vasimvahabov.stockmarketsimulator.service;
+
+import com.vasimvahabov.stockmarketsimulator.entity.QuotePublishCheckpoint;
+
+public interface QuotePublishCheckpointService {
+
+    void saveQuotePublishCheckpoint(QuotePublishCheckpoint quotePublishCheckpoint);
+
+}
---
+++ src/main/java/com/vasimvahabov/stockmarketsimulator/service/impl/QuotePublishCheckpointServiceImpl.java
@@ -0,0 +1,25 @@
+package com.vasimvahabov.stockmarketsimulator.service.impl;
+
+import com.vasimvahabov.stockmarketsimulator.entity.QuotePublishCheckpoint;
+import com.vasimvahabov.stockmarketsimulator.repository.QuotePublishCheckpointRepository;
+import com.vasimvahabov.stockmarketsimulator.service.QuotePublishCheckpointService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class QuotePublishCheckpointServiceImpl implements QuotePublishCheckpointService {
+
+    QuotePublishCheckpointRepository checkpointRepository;
+
+    @Override
+    public void saveQuotePublishCheckpoint(QuotePublishCheckpoint checkpoint) {
+        checkpointRepository.save(checkpoint);
+    }
+
+}
