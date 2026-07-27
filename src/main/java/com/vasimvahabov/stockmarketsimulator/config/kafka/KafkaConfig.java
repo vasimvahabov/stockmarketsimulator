a66d574c978416e183a8049015616b616f8d98e7
Vasim <vahabov.vasim@gmail.com>
2026-07-27 16:10:59 +0200

    feat(kafka): configure ConsumerRecordRecoverer to log after max retries exhausted

--- src/main/java/com/vasimvahabov/stockmarketsimulator/config/kafka/KafkaConfig.java
+++ src/main/java/com/vasimvahabov/stockmarketsimulator/config/kafka/KafkaConfig.java
@@ -1,14 +1,17 @@
 package com.vasimvahabov.stockmarketsimulator.config.kafka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.vasimvahabov.stockmarketsimulator.config.kafka.KafkaProps.KafkaTopicProp;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.listener.ConsumerRecordRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries;
 import org.springframework.util.backoff.ExponentialBackOff;
 
+@Slf4j
 @Configuration
 public class KafkaConfig {
 
@@ -27,7 +30,16 @@ public class KafkaConfig {
         backOff.setInitialInterval(1_000);
         backOff.setMultiplier(2);
         backOff.setMaxInterval(10_000);
-        return new DefaultErrorHandler(backOff);
+
+        ConsumerRecordRecoverer recoverer = (record, exception) -> log.error(
+                "Retries exhausted for record - Topic: {}, Partition: {}, Offset: {}",
+                record.topic(),
+                record.partition(),
+                record.offset(),
+                exception
+        );
+        return new DefaultErrorHandler(recoverer, backOff);
+
     }
 
 }
