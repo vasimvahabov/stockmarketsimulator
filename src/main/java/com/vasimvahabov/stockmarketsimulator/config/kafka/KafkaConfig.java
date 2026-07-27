9ee4dfa551576bdd2317b899557ee37ef2e447a1
Vasim <vahabov.vasim@gmail.com>
2026-07-27 17:52:18 +0200

    feat(kafka): implement custom dead letter topic recovery with detailed logging after max retries

--- src/main/java/com/vasimvahabov/stockmarketsimulator/config/kafka/KafkaConfig.java
+++ src/main/java/com/vasimvahabov/stockmarketsimulator/config/kafka/KafkaConfig.java
@@ -2,11 +2,14 @@ package com.vasimvahabov.stockmarketsimulator.config.kafka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.vasimvahabov.stockmarketsimulator.config.kafka.KafkaProps.KafkaTopicProp;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.ConsumerRecordRecoverer;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries;
 import org.springframework.util.backoff.ExponentialBackOff;
@@ -25,19 +28,26 @@ public class KafkaConfig {
     }
 
     @Bean
-    DefaultErrorHandler defaultErrorHandler() {
+    DefaultErrorHandler defaultErrorHandler(KafkaTemplate<Object, Object> kafkaTemplate, KafkaProps kafkaProps) {
         ExponentialBackOff backOff = new ExponentialBackOffWithMaxRetries(10);
         backOff.setInitialInterval(1_000);
         backOff.setMultiplier(2);
         backOff.setMaxInterval(10_000);
 
-        ConsumerRecordRecoverer recoverer = (record, exception) -> log.error(
-                "Retries exhausted for record - Topic: {}, Partition: {}, Offset: {}",
-                record.topic(),
-                record.partition(),
-                record.offset(),
-                exception
-        );
+        ConsumerRecordRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate, (record, exception) -> {
+            log.error(
+                    "Retries exhausted after {} attempts for record - Topic: {}, Partition: {}, Offset: {}",
+                    backOff.getMaxAttempts(),
+                    record.topic(),
+                    record.partition(),
+                    record.offset(),
+                    exception
+            );
+            return new TopicPartition(
+                    record.topic() + kafkaProps.dltSuffix(),
+                    record.partition()
+            );
+        });
         return new DefaultErrorHandler(recoverer, backOff);
 
     }
